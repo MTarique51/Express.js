3935b979bdfb44228d7c868e8b8bec302ac1bb5d
MTarique51 <mohdtarique7223@gmail.com>
2026-08-21 17:27:16 +0530

    add index.js file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
  const express = require("express");
  const app = express();
 
- let port = 3000;   // 8000
+ let port = 8000;
 
  app.listen(port, () => {
     console.log(`app is listening on port ${port}`);
