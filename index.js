b966af531245cbc66fdb73ecb73f80d21fc82eb7
MTarique51 <mohdtarique7223@gmail.com>
2026-08-22 10:40:55 +0530

    Request and responce

--- index.js
+++ index.js
@@ -6,3 +6,17 @@
  app.listen(port, () => {
     console.log(`app is listening on port ${port}`);
  });
+
+ app.use((req , res) => {
+   // console.log(req);
+   console.log("Request received");
+   // res.send("this is a basic responce")
+
+   // res.send({
+   //    name:"apple",
+   //    color: "red",
+   // });
+
+   let code = "<h1>Fruits</h1> <ul><li>apple</li><li>orange</li></ul>";
+   res.send(code);
+ });
