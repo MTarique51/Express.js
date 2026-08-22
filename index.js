699de90196e8d97f06c1d21ce163946a1e06eab4
MTarique51 <mohdtarique7223@gmail.com>
2026-08-22 10:56:52 +0530

    Routing (get & post)

--- index.js
+++ index.js
@@ -7,16 +7,41 @@
     console.log(`app is listening on port ${port}`);
  });
 
- app.use((req , res) => {
-   // console.log(req);
-   console.log("Request received");
-   // res.send("this is a basic responce")
+//  app.use((req , res) => {
+//    // console.log(req);
+//    // console.log("Request received");
+//    // res.send("this is a basic responce")
 
-   // res.send({
-   //    name:"apple",
-   //    color: "red",
-   // });
+//    // res.send({
+//    //    name:"apple",
+//    //    color: "red",
+//    // });
 
-   let code = "<h1>Fruits</h1> <ul><li>apple</li><li>orange</li></ul>";
-   res.send(code);
- });
+//    let code = "<h1>Fruits</h1> <ul><li>apple</li><li>orange</li></ul>";
+//    res.send(code);
+//  });
+
+
+   //  for Routing
+
+   // gor get 
+app.get("/", (req, res) => {
+   res.send("you contacted root path");
+});
+
+app.get("/apple", (req, res) => {
+   res.send("you contacted apple path");
+});
+
+app.get("/orange", (req, res) => {
+   res.send("you contacted orange path");
+});
+
+app.get("*", (req, res) => {
+   res.send("This path doesn't exist");
+});
+
+   // for post
+app.post("/", (req, res) => {
+   res.send("you sent a post to root path");
+});
